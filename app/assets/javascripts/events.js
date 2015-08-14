6738980d8aa136f1f4a15a6b4f28a66be66590c8
Eric Turner <ericturnerdev@gmail.com>
2015-08-14 22:46:46 +0900

    Prevent soft keyboard open on iPhone

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -2,3 +2,14 @@ $(function () {
   $('#datetimepicker1').datetimepicker();
 });
 
+$("#datepicker1").datepicker({
+    showOn: 'button',
+    onClose: function(dateText, inst)
+    {
+        $(this).attr("disabled", false);
+    },
+    beforeShow: function(input, inst)
+    {
+        $(this).attr("disabled", true);
+    }
+});
