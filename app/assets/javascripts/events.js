8adf5456c656200cfa706d0c72037820517acab8
Eric Turner <ericturnerdev@gmail.com>
2015-08-14 15:26:55 +0000

    Update events/new

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -2,14 +2,3 @@ $(function () {
   $('#datetimepicker1').datetimepicker();
 });
 
-$("#datepicker1").datepicker({
-    showOn: 'button',
-    onClose: function(dateText, inst)
-    {
-        $(this).attr("disabled", false);
-    },
-    beforeShow: function(input, inst)
-    {
-        $(this).attr("disabled", true);
-    }
-});
