4f63e0215d070f8614e216f5e8548e47f7954859
Eric Turner <ericturnerdev@gmail.com>
2015-11-03 14:21:19 +0000

    Update tests

--- spec/features/user_adds_event_spec.rb
+++ spec/features/user_adds_event_spec.rb
@@ -9,6 +9,6 @@ RSpec.feature "User adds new event" do
     fill_in "event[remote_time]", with: "02/03/2015 2:29 PM"
     fill_in "event[local_location]", with: "Los Angeles, CA"
     click_button "Calculate"
-    expect(page).to have_content("Success!")
+    expect(page).to have_content("2015-02-02 21:29:00 +0000 in Los Angeles, CA")
   end
 end
